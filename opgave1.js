7ad796b82761bdcc6457da364595b64d5dd9841b
Tina Wulff <tina@tinawulff.dk>
2025-02-03 13:33:43 +0100

    opgave 1

---
+++ opgave1.js
@@ -0,0 +1,17 @@
+let person = {
+    firstname: "Tina",
+    lastname: "Wulff",
+    age: 34,
+    numberOfChildren: 1,
+    childName: "Asta",
+    city: "Copenhagen"
+}
+
+console.log(person.firstname);
+console.log(person["firstname"]);
+
+console.log(person.numberOfChildren);
+console.log(person["numberOfChildren"]);
+
+console.log(person.lastname);
+console.log(person["lastname"]);
