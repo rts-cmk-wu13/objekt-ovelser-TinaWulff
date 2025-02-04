84ce9e5d5709db9fc55385112af895e9a3fac738
Tina Wulff <tina@tinawulff.dk>
2025-02-04 09:41:41 +0100

    opgave 3 done

--- opgave3.js
+++ opgave3.js
@@ -1,15 +1,37 @@
+
+
 let students = [
     { name: "Sofie", age: 25, course: "Webudvikler" },
-    { name: "Lukas", age: 22, course: "m´Mediegrafiker" },
+    { name: "Lukas", age: 22, course: "Mediegrafiker" },
     { name: "Emil", age: 28, course: "Teknisk designer" },
 ];
 
-let studentListdiv = document.querySelector(".student-list");
+let studentListDiv = document.querySelector(".student-list");
+
 
 students.forEach(function(student) {
-    const studentDiv = document.createElement("div");
-
-    studentDiv.textContent =
-    student.name + ', Alder: ' + stude
+    let studentDiv = document.createElement("div");
+    studentDiv.classList.add("student")
+    studentDiv.textContent = `${student.name}, Alder: ${student.age}, Uddannelse: ${student.course}`;
+    studentListDiv.appendChild(studentDiv);
+    // student.name + ', Alder: ' + student.age + 'Uddannelse:' + student.course;
+  
 });
+
+
+students.forEach(student => {
+    let studentDiv = document.createElement("div");
+    studentDiv.classList.add("student")
+    let studentHeadline = document.createElement("h2");
+    studentHeadline.textContent = student.name;
+    let studentInfo = document.createElement("p")
+    studentInfo.textContent = `Alder: ${student.age}, Uddannelse: ${student.course}`;
+    studentDiv.appendChild(studentHeadline);
+    studentDiv.appendChild(studentInfo)
+    studentListDiv.appendChild(studentDiv);
+});
+
+
+
+
 
